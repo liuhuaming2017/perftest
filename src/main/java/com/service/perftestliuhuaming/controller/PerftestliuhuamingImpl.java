aa036300c177bd0237f00646c383038603761e5b
paas <paas@cse-codegen-backend-3005643035-bxx94>
2018-06-20 07:14:26 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/perftestliuhuaming/controller/PerftestliuhuamingImpl.java
@@ -0,0 +1,28 @@
+package com.service.perftestliuhuaming.controller;
+
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import org.apache.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringDemoCodegen", date = "2018-06-20T07:14:26.811Z")
+
+@RestSchema(schemaId = "perftestliuhuaming")
+@RequestMapping(path = "/perf-test-liuhuaming", produces = MediaType.APPLICATION_JSON)
+public class PerftestliuhuamingImpl {
+
+    @Autowired
+    private PerftestliuhuamingDelegate userPerftestliuhuamingDelegate;
+
+
+    @RequestMapping(value = "/helloworld",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String helloworld( @RequestParam(value = "name", required = true) String name){
+
+        return userPerftestliuhuamingDelegate.helloworld(name);
+    }
+
+}
