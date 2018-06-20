283af6470a53c55aef9a037c8a6194fa9f24af4a
paas <paas@cse-codegen-backend-3005643035-79bz0>
2018-06-20 07:50:07 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/perftestliuhuaming/controller/PerftestliuhuamingDelegate.java
@@ -0,0 +1,14 @@
+package com.service.perftestliuhuaming.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PerftestliuhuamingDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
---
+++ src/test/java/com/service/perftestliuhuaming/controller/TestPerftestliuhuaming.java
@@ -0,0 +1,24 @@
+package com.service.perftestliuhuaming.controller;
+
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestPerftestliuhuaming {
+
+        PerftestliuhuamingDelegate perftestliuhuamingDelegate = new PerftestliuhuamingDelegate();
+
+
+    @Test
+    public void testhelloworld(){
+
+        String expactReturnValue = "hello"; // You should put the expect String type value here.
+
+        String returnValue = perftestliuhuamingDelegate.helloworld("hello");
+
+        assertEquals(expactReturnValue, returnValue);
+    }
+
+}
