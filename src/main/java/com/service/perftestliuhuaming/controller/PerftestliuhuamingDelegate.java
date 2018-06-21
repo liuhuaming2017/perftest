abf34947933ff7aee196ed2364a8d022ffd5b47a
liuhuaming2017 <33945540+liuhuaming2017@users.noreply.github.com>
2018-06-21 03:20:08 +0000

    Update PerftestliuhuamingDelegate.java

--- src/main/java/com/service/perftestliuhuaming/controller/PerftestliuhuamingDelegate.java
+++ src/main/java/com/service/perftestliuhuaming/controller/PerftestliuhuamingDelegate.java
@@ -2,7 +2,8 @@ package com.service.perftestliuhuaming.controller;
 
 import org.apache.servicecomb.serviceregistry.RegistryUtils;
 import org.springframework.stereotype.Component;
-
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class PerftestliuhuamingDelegate {
@@ -10,6 +11,5 @@ public class PerftestliuhuamingDelegate {
     public String helloworld(String name){
         // Do Some Magic Here!
         System.out.println("test");
-        return RegistryUtils.getPublishAddress() + name;
     }
 }
